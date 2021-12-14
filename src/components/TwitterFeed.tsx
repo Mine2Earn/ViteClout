f858159743898ede6f837364b2c5bcc959f28c83
ekazukii <baptisteloison8400@gmail.com>
2021-12-14 17:58:34 +0100

    Update twitterFeed with the good vuilder

--- src/components/TwitterFeed.tsx
+++ src/components/TwitterFeed.tsx
@@ -1,7 +1,7 @@
-export default function TwitterFeed() {
+export default function TwitterFeed({ twttag }: { twttag: string }) {
     return (
-        <a className="twitter-timeline" data-width="600" data-height="500" data-theme="dark" href="https://twitter.com/elonmusk?ref_src=twsrc%5Etfw">
-            Tweets by elonmusk
+        <a className="twitter-timeline" data-width="600" data-height="500" data-theme="dark" href={`https://twitter.com/${twttag}?ref_src=twsrc%5Etfw`}>
+            Tweets by ${twttag}
         </a>
     );
 }
