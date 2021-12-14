a6435f6cae8493bbe35d0c7c4ac34e6d73b099d6
ekazukii <baptisteloison8400@gmail.com>
2021-12-14 18:59:37 +0100

    Change VuilderBlock to redirect to vuilder's page

--- src/components/VuilderBlock.tsx
+++ src/components/VuilderBlock.tsx
@@ -1,20 +1,26 @@
 import styled from 'styled-components';
 import Container from './Container';
 import ProfilePicture from './ProfilePicture';
+import { useNavigate } from 'react-router-dom';
 
 const StyledBlock = styled.div`
     text-align: center;
 `;
 
-//TODO: Button redirect to Vuilder's page
 export default function VuilderBlock({ twttag }: { twttag: string }) {
+    const navigate = useNavigate();
+
+    const onClick = () => {
+        navigate(`/vuilder/${twttag}`);
+    };
+
     return (
         <Container bgcolor={'#292F34'}>
             <StyledBlock>
-                <p>@twttag</p>
+                <p>@{twttag}</p>
                 <ProfilePicture twttag={twttag}></ProfilePicture>
                 <p>
-                    <button>Learn more</button>
+                    <button onClick={onClick}>Learn more</button>
                 </p>
             </StyledBlock>
         </Container>
