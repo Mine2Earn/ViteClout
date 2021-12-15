99047facb4a7bdbe2d9aa9542d6cbda2ddd2bfd7
ekazukii <baptisteloison8400@gmail.com>
2021-12-15 12:58:52 +0100

    Query PP and bio from back

--- src/components/ProfileDescription.tsx
+++ src/components/ProfileDescription.tsx
@@ -1,4 +1,7 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { APIHOST } from '../config';
 
 const Desc = styled.div`
     margin: 10px 20px;
@@ -6,12 +9,22 @@ const Desc = styled.div`
 
 //TODO: Connect to profile picture endpoint
 export default function ProfileDescription(props: { twttag: string; size?: string }) {
+    const [bio, setBio] = useState('LE NULLOS IS A PAS DE BIO');
+
+    useEffect(() => {
+        axios
+            .get(`${APIHOST}/vuilders/infofromtwt?twitter_tag=["${props.twttag}"]`)
+            .then(res => {
+                if (res.status === 200 && res.data.message === 'Ok') {
+                    if (!res.data.vuilders.length) return;
+                    setBio(res.data.vuilders[0].bio);
+                }
+            })
+            .catch(console.error);
+    }, [props.twttag]);
     return (
         <Desc>
-            <cite>
-                Dans l'idée ici ce serait la description que l'user a rentré. Ou s'il n'en n'a pas rentré sa bio twitter Dans l'idée ici ce serait la description que
-                l'user a rentré. Ou s'il n'en n'a pas rentré sa bio twitter
-            </cite>
+            <cite>{bio}</cite>
         </Desc>
     );
 }
--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,4 +1,7 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { APIHOST } from '../config';
 
 const StyledImage = styled.img`
     width: ${(p: { size?: string }) => (p.size ? p.size : '150px')};
@@ -7,9 +10,22 @@ const StyledImage = styled.img`
 
 //TODO: Connect to profile picture endpoint
 export default function ProfilePicture(props: { twttag: string; size?: string }) {
+    const [imgSrc, setImgSrc] = useState('https://via.placeholder.com/150');
+
+    useEffect(() => {
+        axios
+            .get(`${APIHOST}/vuilders/infofromtwt?twitter_tag=["${props.twttag}"]`)
+            .then(res => {
+                if (res.status === 200 && res.data.message === 'Ok') {
+                    if (!res.data.vuilders.length) return;
+                    setImgSrc(res.data.vuilders[0].avatar);
+                }
+            })
+            .catch(console.error);
+    }, [props.twttag]);
     return (
         <>
-            <StyledImage src="https://via.placeholder.com/150" alt={`profile picture of ${props.twttag}`} size={props.size} />
+            <StyledImage src={imgSrc} alt={`profile picture of ${props.twttag}`} size={props.size} />
         </>
     );
 }
