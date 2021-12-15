13b7edba1ad0baca9d34228b5ced7d335762d1f6
Mathis Beauville <mathis.beauville@gmail.com>
2021-12-15 20:34:15 +0100

    Add certification for vuilders #43

--- src/pages/Vuilder.tsx
+++ src/pages/Vuilder.tsx
@@ -10,6 +10,10 @@ import VFTTradeButton from '../components/VFTTradeButton';
 import VuilderInfo from '../components/VuilderInfo';
 import TwitterFeed from '../components/TwitterFeed';
 import { useVuilderAddress } from '../hooks/useVuilder';
+import badge from '../Twitter_Verified_Badge.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { APIHOST } from '../config';
 
 const FlexContainer = styled.div`
     display: flex;
@@ -32,14 +36,29 @@ const FloatRight = styled.div`
     margin-left: auto;
 `;
 
+const BadgeSize = styled.img`
+    margin-left: 15px;
+    width: 30px;
+`;
+
 //TODO: Add the twitter banner of the account
 export default function Vuilder(props: { twttag: string }) {
     const address = useVuilderAddress(props.twttag);
+    const [isVuilder, setIsVuilder] = useState(false);
+
+    useEffect(() => {
+        axios.get(`${APIHOST}/vuilders/isvuilder?twitter_tag=${props.twttag}`).then(res => {
+            setIsVuilder(res.data.isVuilder);
+        });
+    }, []);
 
     return (
         <>
             <Navbar></Navbar>
-            <Title size={2}>Elon Musk - {props.twttag}</Title>
+            <Title size={2}>
+                Elon Musk - {props.twttag}
+                {!!isVuilder && <BadgeSize src={badge} />}
+            </Title>
 
             <FlexContainer>
                 <Container mright={'none'} mleft={'none'}>
